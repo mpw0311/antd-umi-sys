2939e93a1e20b6f4704991b05baaa95fdfcc9d12
“‘mpw <mpw0311@163.com>
2018-09-14 10:41:51 +0800

    下载地址更改

--- src/components/GlobalHeader/Download.js
+++ src/components/GlobalHeader/Download.js
@@ -1,10 +1,10 @@
 import { Icon } from 'antd';
 import { Link } from 'dva/router';
+import { xlightning } from 'config';
 
 function Download(props) {
     const { userInfo } = props;
     const { userName } = userInfo;
-    const xlightning = document.head.dataset.api || "http://xlightning.jianlc.cn";
     return (
         <Link to={{ pathname: "/sys/frame", state: { userName, pathtitles: ["下载任务列表"], url: `${xlightning}/downloadTask/toDownLoad` } }} style={{ color: "#fff" }}>
             <Icon type="download" /> 下载
