869b6ae7c5fbbe0c2dc0797a24d4b690de7ba89c
“‘mpw <mpw0311@163.com>
2018-09-18 16:18:34 +0800

    注册页跳转

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -67,7 +67,7 @@ class Login extends Component {
                         <Button type="primary" htmlType="submit" className={styles.login_form_button} loading={loading}>
                             登录
                        </Button>
-                        {/* 或<a href="#register">注册账户!</a> */}
+                        或<a href="/register">注册账户!</a>
                     </FormItem>
                 </Form>
             </div>
