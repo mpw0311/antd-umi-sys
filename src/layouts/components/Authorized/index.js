c70154c860f64d9cc5fb30889d1b94bab78ca352
“‘mpw <mpw0311@163.com>
2019-01-29 13:09:22 +0800

    Authorized

--- src/layouts/components/Authorized/index.js
+++ src/layouts/components/Authorized/index.js
@@ -1,8 +1,9 @@
 import { PureComponent } from 'react';
+import { Page } from '@components';
 export default class Authorized extends PureComponent {
     render() {
         const { children, noMatch, diffMenuData, location: { pathname } } = this.props;
         const [res] = diffMenuData.filter(item => item.link === pathname);
-        return res ? noMatch : children;
+        return (<Page showHeader={false}>{res ? noMatch : children}</Page>);
     }
 }
