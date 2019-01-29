f0e59b606db252b63f883e4c2ede36e0f87a5812
“‘mpw <mpw0311@163.com>
2019-01-29 14:20:36 +0800

    权限路由控制

--- src/layouts/components/Authorized/index.js
+++ src/layouts/components/Authorized/index.js
@@ -4,6 +4,6 @@ export default class Authorized extends PureComponent {
     render() {
         const { children, noMatch, diffMenuData, location: { pathname } } = this.props;
         const [res] = diffMenuData.filter(item => item.link === pathname);
-        return res ? (<Page showHeader={false}>{noMatch}</Page>) : children;
+        return res ? (<Page showHeader={false} flex={true}>{noMatch}</Page>) : children;
     }
 }
