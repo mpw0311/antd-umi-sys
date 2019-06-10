1e69a675f91cf804424ddfc32911ea5f88a53dc3
maopengwei <maopengwei@jianlc.com>
2019-06-10 11:04:36 +0800

    Stargazers

--- src/pages/sys/github/$id/index.js
+++ src/pages/sys/github/$id/index.js
@@ -5,68 +5,63 @@ import { Page } from '@components';
 import { Line } from '@components/Echarts';
 import RepoStargazers from '../components/repoStargazers';
 @connect(({ github, loading }) => {
-    const {
-        account,
-        stars,
-        currentRepoName,
-        description
-    } = github
-    return {
-        account,
-        stars,
-        currentRepoName,
-        description,
-        loading: loading.models.github
-    }
+  const { account, stars, currentRepoName, description } = github;
+  return {
+    account,
+    stars,
+    currentRepoName,
+    description,
+    loading: loading.models.github,
+  };
 })
 class Repo extends PureComponent {
-    componentDidMount() {
-        const { account, currentRepoName, repos } = this.props;
-        this.getRepoStars(account, currentRepoName);
+  componentDidMount() {
+    const { account, currentRepoName, repos } = this.props;
+    //stargazers Analysis
+    this.getRepoStars(account, currentRepoName);
+  }
+  componentDidUpdate(nextProps, nextState) {
+    const { account, currentRepoName } = nextProps;
+    if (account !== this.props.account || currentRepoName !== this.props.currentRepoName) {
+      //stargazers Analysis
+      this.getRepoStars(account, currentRepoName);
     }
-    componentDidUpdate(nextProps, nextState) {
-        const { account, currentRepoName } = nextProps;
-        if (account !== this.props.account || currentRepoName !== this.props.currentRepoName) {
-            this.getRepoStars(account, currentRepoName);
-        }
-    }
-    /**
-     * 数据请求
-     */
-    getRepoStars = (account, repoName) => {
-        this.props.dispatch({
-            type: 'github/getReposStars',
-            payload: {
-                account,
-                repoName
-            }
-        });
-    }
-    render() {
-        const { account, currentRepoName, description, stars } = this.props;
-        return (
-            <Page
-                loading={false}
-                pathtitles={[
-                    {
-                        title: 'gitDataV',
-                        link: '/sys/github',
-                        icon: 'github',
-                        state: { account }
-                    },
-                    currentRepoName
-                ]}
-                title={currentRepoName}
-                description={description}
-            >
-                <Card
-                    title="stargazers Analysis"
-                >
-                    <Line data={stars} seriesLayoutBy={"column"} height={400} />
-                </Card>
-                <RepoStargazers />
-            </Page>
-        );
-    }
+  }
+  /**
+   * 数据请求
+   */
+  getRepoStars = (account, repoName) => {
+    this.props.dispatch({
+      type: 'github/getReposStars',
+      payload: {
+        account,
+        repoName,
+      },
+    });
+  };
+  render() {
+    const { account, currentRepoName, description, stars, location, loading } = this.props;
+    return (
+      <Page
+        loading={false}
+        pathtitles={[
+          {
+            title: 'gitDataV',
+            link: '/sys/github',
+            icon: 'github',
+            state: { account },
+          },
+          currentRepoName,
+        ]}
+        title={currentRepoName}
+        description={description}
+      >
+        <Card title="stargazers Analysis">
+          <Line data={stars} seriesLayoutBy={'column'} height={400} loading={loading} />
+        </Card>
+        <RepoStargazers location={location} />
+      </Page>
+    );
+  }
 }
 export default Repo;
--- src/pages/sys/github/components/repoStargazers.js
+++ src/pages/sys/github/components/repoStargazers.js
@@ -4,176 +4,181 @@ import { Link } from 'umi';
 import { Card, Avatar } from 'antd';
 import { formatMessage } from 'umi/locale';
 import Table from './Table';
+
 @connect(({ github, loading }) => {
-    const {
-        account,
-        currentRepoName,
-        repos,
-        currentRepo,
-        stargazersInfo
-    } = github;
-    return {
-        account,
-        currentRepoName,
-        repos,
-        currentRepo,
-        stargazersInfo,
-        loading: loading.models.github
-    };
+  const { account, currentRepoName, repos, currentRepo, stargazersInfo } = github;
+  return {
+    account,
+    currentRepoName,
+    repos,
+    currentRepo,
+    stargazersInfo,
+    loading: loading.models.github,
+  };
 })
 class RepoStargazers extends PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = {
-
-        };
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  static getDerivedStateFromProps(nextProps, prevState) {
+    const {
+      location: {
+        query: { _n: reposName },
+      },
+      repos,
+    } = nextProps;
+    if (prevState.currentRepo === undefined) {
+      const [currentRepo = {}] = repos.filter(item => item.name === reposName);
+      return {
+        ...prevState,
+        currentRepo,
+      };
     }
-    componentDidMount() {
-        const { currentRepo } = this.props;
-        //如果项目列表信息存在
-        if (currentRepo && currentRepo.length > 0) {
-            this.props.dispatch({
-                type: "github/save",
-                payload: {
-                    currentRepo: this.getCurrentRepo()
-                }
-            });
-        }
+    return prevState;
+  }
+  componentDidMount() {
+    const { currentRepo } = this.state;
+    const { stargazersInfo = {} } = this.props;
+    const { url } = stargazersInfo;
+    const { stargazers_url, stargazers_count } = currentRepo;
+    //请求list列表
+    if (stargazers_url && stargazers_url !== url) {
+      const current = Math.ceil(stargazers_count / 10);
+      this.getStargazers(stargazers_url, current);
     }
-    handleChange = (pagination) => {
-        const { currentRepo } = this.props;
-        if (currentRepo && currentRepo.stargazers_url) {
-            const { stargazers_url, stargazers_count } = currentRepo;
-            const { current, pageSize } = pagination;
-            this.getStargazers(stargazers_url, current, pageSize);
-        }
+  }
+  //table切换
+  handleChange = pagination => {
+    const { currentRepo } = this.state;
+    if (currentRepo && currentRepo.stargazers_url) {
+      const { stargazers_url } = currentRepo;
+      const { current, pageSize } = pagination;
+      this.getStargazers(stargazers_url, current, pageSize);
     }
-    //获取当前项目信息
-    getCurrentRepo = () => {
-        const { repos, currentRepoName } = this.props;
-        const [currentRepo = {}] = repos.filter(item => item.name === currentRepoName);
-        return currentRepo;
-    }
-    //获取table列表
-    getStargazers = (stargazers_url, current = 1, pageSize = 10) => {
-        this.props.dispatch({
-            type: 'github/getStargazers',
-            payload: {
-                url: stargazers_url + `?page=${current}&per_page=${pageSize}`,
-            }
-        });
-    }
-    //过滤数据
-    filterData = (stargazersInfo) => {
-        if (!stargazersInfo || stargazersInfo.length === 0) return;
+  };
+  //获取table列表
+  getStargazers = (stargazers_url, current = 1, pageSize = 10) => {
+    this.props.dispatch({
+      type: 'github/getStargazers',
+      payload: {
+        stargazers_url,
+        current,
+        pageSize,
+        url: stargazers_url + `?page=${current}&per_page=${pageSize}`
+      },
+    });
+  };
+  //过滤数据
+  filterData = stargazersList => {
+    if (!stargazersList || stargazersList.length === 0) return;
+    return {
+      columns: [
+        {
+          field: 'avatar_url',
+          name: 'account',
+          type: 'string',
+          colSpan: 2,
+          render: text => <Avatar src={text} />,
+        },
+        {
+          field: 'login',
+          name: 'login',
+          type: 'string',
+          colSpan: 0,
+          render: text => (
+            <Link to={{ pathname: '/sys/github', state: { account: text } }}>{text}</Link>
+          ),
+        },
+        {
+          field: 'name',
+          name: 'name',
+          type: 'string',
+        },
+        {
+          field: 'location',
+          name: 'location',
+          type: 'string',
+        },
+        {
+          field: 'company',
+          name: 'company',
+          type: 'string',
+        },
+        {
+          field: 'followers',
+          name: 'followers',
+          type: 'string',
+        },
+        {
+          field: 'following',
+          name: 'following',
+          type: 'string',
+        },
+        {
+          field: 'public_repos',
+          name: 'public_repos',
+          type: 'string',
+        },
+        {
+          field: 'created_at',
+          name: 'created_at',
+          type: 'string',
+        },
+        {
+          field: 'updated_at',
+          name: 'updated_at',
+          type: 'string',
+        },
+      ],
+      rows: stargazersList.map(item => {
+        const {
+          avatar_url,
+          login,
+          name,
+          location,
+          company,
+          followers,
+          following,
+          public_repos,
+          created_at,
+          updated_at,
+        } = item;
         return {
-            columns: [
-                {
-                    "field": "avatar_url",
-                    "name": "account",
-                    "type": "string",
-                    colSpan: 2,
-                    render: text => (<Avatar src={text} />)
-                },
-                {
-                    "field": "login",
-                    "name": "login",
-                    "type": "string",
-                    colSpan: 0,
-                    render: text => (<Link to={{ pathname: '/sys/github', state: { account: text } }} >{text}</Link>)
-                },
-                {
-                    "field": "name",
-                    "name": "name",
-                    "type": "string",
-                },
-                {
-                    "field": "location",
-                    "name": "location",
-                    "type": "string",
-                },
-                {
-                    "field": "company",
-                    "name": "company",
-                    "type": "string",
-                },
-                {
-                    "field": "followers",
-                    "name": "followers",
-                    "type": "string"
-                },
-                {
-                    "field": "following",
-                    "name": "following",
-                    "type": "string"
-                },
-                {
-                    "field": "public_repos",
-                    "name": "public_repos",
-                    "type": "string"
-                },
-                {
-                    "field": "created_at",
-                    "name": "created_at",
-                    "type": "string"
-                },
-                {
-                    "field": "updated_at",
-                    "name": "updated_at",
-                    "type": "string"
-                },
-
-            ],
-            rows: stargazersInfo.map(item => {
-                const {
-                    avatar_url,
-                    login,
-                    name,
-                    location,
-                    company,
-                    followers,
-                    following,
-                    public_repos,
-                    created_at,
-                    updated_at,
-
-                } = item;
-                return {
-                    avatar_url,
-                    login,
-                    name,
-                    location,
-                    company,
-                    followers,
-                    following,
-                    public_repos,
-                    created_at,
-                    updated_at,
-                };
-            })
-            // .sort((a, b) => b.followers - a.followers)
+          avatar_url,
+          login,
+          name,
+          location,
+          company,
+          followers,
+          following,
+          public_repos,
+          created_at,
+          updated_at,
         };
-    }
-    render() {
-        const { current, loading, stargazersInfo } = this.props;
-        debugger
-        const data = this.filterData(stargazersInfo);
+      }),
+      // .sort((a, b) => b.followers - a.followers)
+    };
+  };
+  render() {
+    const { loading, stargazersInfo = {} } = this.props;
+    const { currentRepo = {} } = this.state;
+    const { stargazers_count } = currentRepo;
+    const { list = [], current } = stargazersInfo;
+    const data = this.filterData(list);
 
-        return (
-            <Card
-                title={formatMessage({ id: "gitDataV.stargazers.list" })}
-                style={{ marginTop: 20 }}
-            >
-                <Table
-                    data={data}
-                    // total={stargazers_count}
-                    onChange={this.handleChange}
-                    loading={loading}
-                    current={current}
-                />
-            </Card>
-        );
-    }
+    return (
+      <Card title={formatMessage({ id: 'gitDataV.stargazers.list' })} style={{ marginTop: 20 }}>
+        <Table
+          data={data}
+          total={stargazers_count}
+          onChange={this.handleChange}
+          loading={loading}
+          current={current}
+        />
+      </Card>
+    );
+  }
 }
 
 export default RepoStargazers;
